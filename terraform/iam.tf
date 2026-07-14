a680930afcfe11916cff8a01625d85389e318c0c
agandman1712-rgb <agandman.1712@gmail.com>
2026-07-15 00:18:49 +0300

    Update iam.tf
    "://.amazonaws.com" to "eks.amazonaws.com"

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -8,7 +8,7 @@ resource "aws_iam_role" "eks_cluster_role" {
         Action = "sts:AssumeRole"
         Effect = "Allow"
         Principal = {
-          Service = "://amazonaws.com"
+          Service = "eks.amazonaws.com"
         }
       },
     ]
