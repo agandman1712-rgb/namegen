eab035b54c6b3161fbfced2e5cee3b9eb30a5739
agandman1712-rgb <agandman.1712@gmail.com>
2026-07-16 22:09:01 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,22 +21,22 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
 
-  name            = "namegen-vpc"
-  cidr            = "10.0.0.0/16"
+  name            = "namegen-vpc-v2"
+  cidr            = "10.10.0.0/16"  
   azs             = slice(data.aws_availability_zones.available.names, 0, 2)
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  private_subnets = ["10.10.1.0/24", "10.10.2.0/24"]
+  public_subnets  = ["10.10.101.0/24", "10.10.102.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
 
   public_subnet_tags  = { 
-    "kubernetes.io/role/elb"                = "1" 
-    "kubernetes.io/cluster/namegen-cluster" = "shared" 
+    "kubernetes.io/role/elb"                   = "1" 
+    "kubernetes.io/cluster/namegen-cluster-v2" = "shared" 
   }
   private_subnet_tags = { 
-    "kubernetes.io/role/internal-elb"       = "1" 
-    "kubernetes.io/cluster/namegen-cluster" = "shared" 
+    "kubernetes.io/role/internal-elb"          = "1" 
+    "kubernetes.io/cluster/namegen-cluster-v2" = "shared" 
   }
 }
 
@@ -44,7 +44,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = "namegen-cluster"
+  cluster_name    = "namegen-cluster-v2" 
   cluster_version = "1.31"
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
@@ -71,7 +71,7 @@ module "eks" {
 }
 
 resource "aws_ecr_repository" "namegen" {
-  name                 = "namegen"
+  name                 = "namegen-v2" 
   image_tag_mutability = "MUTABLE"
   force_delete         = true
 }
